a04695cb253c4ebff3a9b002605338668f519519
Alessandro Catucci <graphic.alessandro@gmail.com>
2024-11-06 23:00:17 +0100

    change folder component in assets

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import "../component/style/style.scss";
+import "../assets/style/style.scss";
 
 //import js api
-import { api } from "../component/js/callApi.js";
-import { list } from "../component/js/listGenre.js";
-import { description } from "../component/js/description.js";
-import { book } from "../component/js/book.js";
+import { api } from "../assets/js/callApi.js";
+import { list } from "../assets/js/listGenre.js";
+import { description } from "../assets/js/description.js";
+import { book } from "../assets/js/book.js";
