c2272723ca03ae7574c165d1fc2336224e781a6b
Alessandro Catucci <graphic.alessandro@gmail.com>
2024-11-28 17:45:42 +0100

    fixed gile webpach

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js',
-    publicPath: '/static/',  // Dove sono serviti gli asset nel browser
+    publicPath: './',  // Dove sono serviti gli asset nel browser
   },
 
   plugins: [
