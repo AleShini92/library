14918baa3e72ff5d3ca658c03d585478f3e66a40
Alessandro Catucci <graphic.alessandro@gmail.com>
2024-11-30 13:08:07 +0100

    fixed bug css file index.html

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '',
+    publicPath: './',
   },
   mode: 'development', // Impostalo su 'production' per la produzione
   module: {
